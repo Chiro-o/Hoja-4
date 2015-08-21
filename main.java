6850555f3feaec72d4691c4dfaa7220206c69dfb
Chiro1 <cris.a.06@hotmail.es>
2015-08-20 20:39:46 -0600

    Update main.java

--- main.java
+++ main.java
@@ -1,15 +1,21 @@
 import java.util.Scanner;
 
 /**
- * @author Christopher Chiroy, Boris Cifuentes
- * @category Hoja de Trabajo 2
+ * @author Christopher Chiroy, Boris cifuentes
+ * Hoja de Trabajo 4
  */
 public class main {
 	
 	public static void main(String args[]){
-		CtrlCalculadora calcu = new CtrlCalculadora();
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("ingrese tipo de pila que desea usar\n");
+		System.out.println("1. ArreyList \n2. Vector \nListas: \n	3. Simplemente Enlazada\n"
+				+ "	4. Doblemente Enlazada \n	5. Circular");
+		int opcion = teclado.nextInt();
 		
-			calcu.readFile("src/datos.txt");
+		CtrlCalculadora calcu = new CtrlCalculadora(opcion);
+		
+			calcu.readFile("datos.txt");
 		try {
 			System.out.println(""+calcu.calcular());
 		} catch (Exception e) {
